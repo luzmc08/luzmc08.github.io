a1fbb1cca6befd7f04d7006ffbe46281f7a7febc
luzmcs <luzmcsanchez@gmail.com>
2021-05-07 23:20:18 -0500

    oki

--- mate/graficolinel.py
+++ mate/graficolinel.py
@@ -1,12 +1,13 @@
-import matplotlib.pyplot as plt 
-x1 = [1,2,3]
-y1 = [5,7,4]
-x2 = [1,2,3]
-y2 = [7,5,6]
-plt.plot(x1,y1, label="tipo 1")
-plt.plot(x2,y2, label="tipo 2")
-plt.xlabel("Eje x")
-plt.ylabel("Eje y")
-plt.tittle("grafico lineal")
+import matplotlib.pyplot as plt
+x1=[1,2,3]
+y1=[5,7,4]
+x2=[1,2,3]
+y2=[7,5,6]
+plt.plot(x1,y1,label='Tipo 1')
+plt.plot(x2,y2,label='Tipo 2') 
+plt.xlabel('Eje X')  
+plt.ylabel('Eje Y')  
+plt.title('Grafica lineal')  
 plt.legend()
 plt.show()
+
