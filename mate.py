6cf0f08600adae5ccecca3a05a9ef98c52fc4582
luzmcs <luzmcsanchez@gmail.com>
2021-05-12 23:45:28 -0500

    oui

---
+++ mate.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+workbook1 = "peliculas.xlsx"
+
+df = pd.read_excel(workbook1)
+print(df.head())
+valores = df[["Title","Runtime (Minutes)"]]
+print(valores)
+ax=valores.plot.bar(x="Title",y="Runtime (Minutes)",
+                    rot=0, color="purple" )
+plt.show()
