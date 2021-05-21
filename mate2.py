a492133416ba08749018ecd2fc196cb8494621e7
luzmcs <luzmcsanchez@gmail.com>
2021-05-20 23:45:52 -0500

    jkxkcn

--- mate2.py
+++ mate2.py
@@ -1,12 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 12 22:12:45 2021
+
+@author: LMCS
+"""
 import pandas as pd
 import matplotlib.pyplot as plt
 
-workbook1 = "DATOS1.xlsx"
-
-df = pd.read_excel(workbook1)
-print(df.head())
-valores = df[["DATOS GENERALES","AÑO"]]
-print(valores)
-ax=valores.plot.bar(x="DATOS GENERALES",y="AÑO",
-                    rot=0, color="orange" )
+df=pd.read_excel ("peliculas.xlsx", index_col="Rank")
+"""df1=df.groupby("year").mean()"""
+df1=df.groupby("Year").agg({"Votes": "sum"})
+print(df1.head(11))
+grouped=df1
+grouped["Votes"].plot.pie()
+plt.title("Tabla Year vs Sumatoria votos")
 plt.show()
---
+++ only_mate.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+libro = "Sala6.xlsx"
+hoja = "MILLONARIOS"
+
+df = pd.read_excel(libro, sheet_name=hoja)
+print(df.head())
+
+valores = df[["Nombre","Edad","New Worth (Billions)"]]
+print(valores)
+
+
+ax=valores.plot(x="Nombre", y=["Edad","New Worth (Billions)"], marker=".")
+
+
+plt.xlabel("Millonarios")
+plt.ylabel("Edad  y Valor Neto (Billones)")
+plt.title("Millonarios en el Mundo")
+plt.grid()
+plt.legend()
+plt.show()
