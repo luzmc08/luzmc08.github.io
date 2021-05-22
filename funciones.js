04983c92a898a7b6a42994b75580f65f65cf4dc3
luzmcs <luzmcsanchez@gmail.com>
2021-05-21 20:37:28 -0500

    askudh

--- funciones.js
+++ funciones.js
@@ -3,4 +3,10 @@ alert("Bienvenido a Certus")
 
 function Mostrar () {
     var miDiv =document.getElementById("bienvenido");
+    if(miDiv.style.display == "none"){
+        miDiv.style.display = "block";
+        }else {
+            //oculto la etiqueta div
+            miDiv.style.display = "none"
+        }
 }
