b3d025fb739e7f72f9edc93f664d281d209efe2f
luzmcs <luzmcsanchez@gmail.com>
2021-05-21 20:39:56 -0500

    fvbfv

--- funciones.js
+++ funciones.js
@@ -2,6 +2,7 @@
 function Mostrar () {
     var miDiv =document.getElementById("bienvenido");
     if(miDiv.style.display == "none"){
+        // Muestro la etiqueta div
         miDiv.style.display = "block";
         }else {
             //oculto la etiqueta div
