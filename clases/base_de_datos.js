e08810becf355a4b383157ac4bb3990093e7046d
Luz María <74826220@certus.edu.pe>
2021-06-19 18:21:19 -0500

    sakjdgbaskj

---
+++ clases/base_de_datos.js
@@ -0,0 +1,9 @@
+function cargarInicio() {
+    const miFormulario = document.querySelector('#MiFormulario')
+    miFormulario.addEventListener("submit", e=>{
+        e.preventDefault();
+
+        const(nombre=miFormulario['nombre'].value,
+        const
+    }
+}
