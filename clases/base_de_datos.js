02cfb57be8b3f102c26c4d92e21757dfe7adeec3
Luz María <74826220@certus.edu.pe>
2021-06-20 19:35:57 -0500

    huuuu

--- clases/base_de_datos.js
+++ clases/base_de_datos.js
@@ -1,9 +1,32 @@
-function cargarInicio() {
-    const miFormulario = document.querySelector('#MiFormulario')
-    miFormulario.addEventListener("submit", e=>{
-        e.preventDefault();
+const db = firebase.firestore()
 
-        const(nombre=miFormulario['nombre'].value,
-        const
-    }
+function cargarInicio(){
+    const miFormulario = document.querySelector('#MiFormulario');
+    
+
+    miFormulario.addEventListener("submit", async(e) => {
+        e.preventDefault();
+        console.log("iniciando listener")
+        const Cnombre = miFormulario['Nombre'].value;
+        const Capellidos = miFormulario['Apellidos'].value;
+        const Cedad = miFormulario['Edad'].value;
+
+        console.log("iniciando listener")
+        const respuesta = await db.collection('personas').doc().set({
+            nombre:Cnombre, apellidos:Capellidos, edad:Cedad
+        })
+        console.log(Cnombre + " - " + Capellidos + " - " + Cedad);
+    })
+
+    const miBoton = document.querySelector('#BtnRecuperar');
+    const getPersonas = () => db.collection ('personas').get();
+
+    miBoton.addEventListener("click",async(e)=>{
+        const consultaPersona = await getPersonas();
+        consultaPersona.forEach(doc => {
+            let data = doc.data();
+            var miReporte = document.getElementById('MiReporte');
+            miReporte.innerHTML += data.nombre + '<br>'
+        })
+    })
 }
